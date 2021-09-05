cab33f66776c7e321a2af1947215e18f816084f6
Stathis Tsirigotis <90151928+StathisTsirigotis@users.noreply.github.com>
2021-09-05 21:45:40 +0300

    Create default.php

---
+++ src/modules/mod_stathis_headerinfo/tmpl/default.php
@@ -0,0 +1,12 @@
+<?php 
+// No direct access
+defined('_JEXEC') or die; 
+?>
+<div class="mostrecentlaws">
+	<div class="mostrecentlawsinner">
+		<h2><?php echo $params->get('header', ''); ?></h2>
+		<p><?php echo $params->get('sub_header', ''); ?>
+	</div>
+
+
+</div>
